ca649ec829a197fb99b196688afddaa45bfb3b85
timweri <ng.duc.tahn@gmail.com>
2018-04-15 06:39:53 -0400

    Log now writes to both a file (named 'main.log') and stdout
    Signed-off-by: timweri <ng.duc.tahn@gmail.com>

--- main.py
+++ main.py
@@ -5,7 +5,8 @@ import threading
 from database.mysql import MySQLConnectionPool
 from htmlscraper.html_scraper import KijijiScraper
 
-logging.basicConfig(stream=sys.stdout, level=logging.INFO, format='%(filename)s: '
+logging.basicConfig(handlers=[logging.FileHandler('main.log'), logging.StreamHandler(sys.stdout)], level=logging.DEBUG,
+                    format='%(filename)s: '
                                                                    '%(levelname)s: '
                                                                    '%(funcName)s(): '
                                                                    '%(lineno)d:\t'
@@ -15,7 +16,7 @@ logger = logging.getLogger(__name__)
 if __name__ == '__main__':
     logger.info('Started')
     scraper = KijijiScraper()
-    pool = MySQLConnectionPool('localhost', 'root', 'xxx', 'testdb1', 2)
+    pool = MySQLConnectionPool('localhost', 'root', 'rakion', 'testdb1', 2)
     with open('urls.in', 'r') as f:
         for line in f:
             scraper.scrape_cat_page_ini(line)
@@ -31,3 +32,9 @@ if __name__ == '__main__':
                 thread.start()
 
     logger.info('Terminated')
+
+'''scraper = KijijiScraper()
+page = requests.get(
+    'https://www.kijiji.ca/v-room-rental-roommate/kitchener-waterloo/500-big-new-bedroom-with-ensuite-washroom-and-free-wifi/1347249296')
+html_tree = html.fromstring(page.content)
+print(scraper.get_listing_title(html_tree))'''
